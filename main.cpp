52dc08a11afde3ef54311493851388a3f090ff84
ted-marozzi <edwardmarozzi@gmail.com>
2022-01-24 09:38:54 +1100

    init

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "math/add.h"
+
+int main()
+{
+    std::cout << "Hello World\n";
+    int result = add(3, 4);
+    std::cout << "The addition of 3 and 4 is " << result << "\n";
+}
